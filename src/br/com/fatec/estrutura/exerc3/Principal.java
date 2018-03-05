ce0025cc223f47f5ae525f2a7e47a860a7a6a74e
Karen Vicente <karenyasmin37@gmail.com>
2018-03-04 21:33:50 -0300

    exerc4

--- src/br/com/fatec/estrutura/exerc3/Principal.java
+++ src/br/com/fatec/estrutura/exerc3/Principal.java
@@ -1,34 +1,32 @@
 package br.com.fatec.estrutura.exerc3;
 
+import br.com.fatec.estrutura.No;
+
 /**
  * @author Karen 4 de mar de 2018
  */
 public class Principal {
 
 	/**
-	 * Exercício 3 – Complete o código a seguir para que seja
-impresso na tela o conteúdo dos elementos da lista
-encadeada.
+	 * Exercício 3 – Complete o código a seguir para que seja impresso na tela o
+	 * conteúdo dos elementos da lista encadeada.
+	 * 
 	 * @param args
 	 */
+	// TODO Auto-generated method stub
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		public static void main(String[] args) {
-			 //número aleatório no intervalo [5,14]
-			 int quant = (int) (Math.random() * 10 + 5);
-			 No inicio = new No();
-			 No apontador = inicio;
-			 inicio.conteudo = quant;
-			 while( quant-- > 1 ) {
-			 apontador.proximo = new No();
-			 apontador = apontador.proximo;
-			 apontador.conteudo = quant;
-			 System.out.println("quant:" + quant);
-			 }
-			 //completar aqui
-			 _____________________________
-			 }
-
+		// número aleatório no intervalo [5,14]
+		int quant = (int) (Math.random() * 10 + 5);
+		No inicio = new No();
+		No apontador = inicio;
+		inicio.conteudo = quant;
+		while (quant-- > 1) {
+			apontador.proximo = new No();
+			apontador = apontador.proximo;
+			apontador.conteudo = quant;
+			System.out.println("quant:" + quant);
+		}
+				
 	}
 
 }
---
+++ src/br/com/fatec/estrutura/exerc4/Principal.java
@@ -0,0 +1,40 @@
+package br.com.fatec.estrutura.exerc4;
+
+import br.com.fatec.estrutura.No;
+
+/**
+ * @author Karen 4 de mar de 2018
+ */
+public class Principal {
+
+	/**
+	 * Exercício 4 – Complete o código a seguir para que seja impresso na tela o
+	 * conteúdo dos elementos da lista encadeada
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nro;
+		No inicio = null, no = null;
+		for (int i = 0; i < 5; i++) {
+			nro = (int) (Math.random() * 10 + 1);
+			if (inicio == null) {
+				no = new No();
+				no.conteudo = nro;
+				inicio = no;
+			} else {
+				no.proximo = new No();
+				no = no.proximo;
+				no.conteudo = nro;
+			}
+			System.out.println("Conteudo: " + no.conteudo);
+		}
+		
+		// completar aqui
+		while (inicio != null) {
+			System.out.println(inicio.conteudo);
+			inicio = inicio.proximo;
+		}
+	}
+
+}
